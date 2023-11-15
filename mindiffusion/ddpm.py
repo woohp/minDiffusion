47c868409f9e808dd2a090f5d658921b5395aeb5
Hui Peng Hu <woohp135@gmail.com>
2023-11-14 22:09:21 -0800

    more refactoring

--- mindiffusion/ddpm.py
+++ mindiffusion/ddpm.py
@@ -4,24 +4,17 @@ from torch import Tensor
 
 
 class DDPM(nn.Module):
-    def __init__(
-        self,
-        eps_model: nn.Module,
-        betas: tuple[float, float],
-        n_T: int,
-        criterion: nn.Module = nn.MSELoss(),
-    ) -> None:
+    def __init__(self, eps_model: nn.Module, beta1: float, beta2: float, n_T: int):
         super(DDPM, self).__init__()
         self.eps_model = eps_model
 
-        beta1, beta2 = betas
         assert beta1 < beta2 < 1.0, "beta1 and beta2 must be in (0, 1)"
 
         beta_t = torch.linspace(beta1, beta2, n_T + 1, dtype=torch.float)
         sqrt_beta_t = torch.sqrt(beta_t)
-        alpha_t = 1 - beta_t
+        alpha_t: Tensor = 1 - beta_t
         log_alpha_t = torch.log(alpha_t)
-        alphabar_t = torch.cumsum(log_alpha_t, dim=0).exp()
+        alphabar_t = torch.cumsum(log_alpha_t, dim=0).exp()  # \bar{\alpha_t}
 
         sqrtab = torch.sqrt(alphabar_t)
         oneover_sqrta = torch.rsqrt(alpha_t)
@@ -29,26 +22,21 @@ class DDPM(nn.Module):
         sqrtmab = torch.sqrt(1 - alphabar_t)
         mab_over_sqrtmab_inv = (1 - alpha_t) / sqrtmab
 
-        self.alpha_t: Tensor
         self.oneover_sqrta: Tensor  # 1/\sqrt{\alpha_t}
         self.sqrt_beta_t: Tensor  # \sqrt{\beta_t}
-        self.alphabar_t: Tensor  # \bar{\alpha_t}
         self.sqrtab: Tensor  # \sqrt{\bar{\alpha_t}}
         self.sqrtmab: Tensor  # \sqrt{1-\bar{\alpha_t}}
-        self.mab_over_sqrtmab: Tensor  # (1-\alpha_t)/\sqrt{1-\bar{\alpha_t}}
+        self.mab_over_sqrtmab_inv: Tensor  # (1-\alpha_t)/\sqrt{1-\bar{\alpha_t}}
 
-        self.register_buffer("alpha_t", alpha_t)  # \alpha_t
         self.register_buffer("oneover_sqrta", oneover_sqrta)  # 1/\sqrt{\alpha_t}
         self.register_buffer("sqrt_beta_t", sqrt_beta_t)  # \sqrt{\beta_t}
-        self.register_buffer("alphabar_t", alphabar_t)  # \bar{\alpha_t}
         self.register_buffer("sqrtab", sqrtab)  # \sqrt{\bar{\alpha_t}}
         self.register_buffer("sqrtmab", sqrtmab)  # \sqrt{1-\bar{\alpha_t}}
-        self.register_buffer("mab_over_sqrtmab", mab_over_sqrtmab_inv)  # (1-\alpha_t)/\sqrt{1-\bar{\alpha_t}}
+        self.register_buffer("mab_over_sqrtmab_inv", mab_over_sqrtmab_inv)  # (1-\alpha_t)/\sqrt{1-\bar{\alpha_t}}
 
         self.n_T = n_T
-        self.criterion = criterion
 
-    def forward(self, x: Tensor, eps: Tensor) -> Tensor:
+    def forward(self, x: Tensor) -> tuple[Tensor, Tensor]:
         """
         Makes forward diffusion x_t, and tries to guess epsilon value from x_t using eps_model.
         This implements Algorithm 1 in the paper.
@@ -56,13 +44,14 @@ class DDPM(nn.Module):
 
         _ts = torch.randint(1, self.n_T + 1, (x.shape[0],), device=x.device)
         # t ~ Uniform(0, n_T)
+        eps = torch.randn_like(x)  # eps ~ N(0, 1)
 
         x_t = (
             self.sqrtab[_ts, None, None, None] * x + self.sqrtmab[_ts, None, None, None] * eps
         )  # This is the x_t, which is sqrt(alphabar) x_0 + sqrt(1-alphabar) * eps
         # We should predict the "error term" from this x_t. Loss is what we return.
 
-        return self.eps_model(x_t, _ts / self.n_T)
+        return self.eps_model(x_t, _ts / self.n_T), eps
 
     def sample(self, n_sample: int, size: tuple[int, ...]) -> Tensor:
         device = self.alpha_t.device
@@ -72,6 +61,6 @@ class DDPM(nn.Module):
         for i in range(self.n_T, 0, -1):
             z = torch.randn(n_sample, *size, device=device) if i > 1 else 0
             eps = self.eps_model(x_i, torch.tensor(i / self.n_T, device=device).expand(n_sample, 1))
-            x_i = self.oneover_sqrta[i] * (x_i - eps * self.mab_over_sqrtmab[i]) + self.sqrt_beta_t[i] * z
+            x_i = self.oneover_sqrta[i] * (x_i - eps * self.mab_over_sqrtmab_inv[i]) + self.sqrt_beta_t[i] * z
 
         return x_i
--- train_cifar10.py
+++ train_cifar10.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import click
 import torch
-from torch import Tensor
+import torch.nn.functional as F
 from torchext import engine
 from torchext.callbacks import Callback, ModelCheckpoint
 from torchvision import transforms
@@ -12,16 +12,16 @@ from mindiffusion.ddpm import DDPM
 from mindiffusion.unet import NaiveUnet
 
 
-def preprocess_data(x: Tensor, y: Tensor) -> tuple[tuple[Tensor, Tensor], Tensor]:
-    eps = torch.randn_like(x)  # eps ~ N(0, 1)
-    return (x, eps), eps
+def loss(output: tuple[torch.Tensor, torch.Tensor], _y: torch.Tensor) -> torch.Tensor:
+    eps_hat, eps = output
+    return F.mse_loss(eps_hat, eps)
 
 
 @click.command()
 @click.option("--n-epoch", type=int, default=100)
 @click.option("--load-pth", type=click.Path())
 def train_cifar10(n_epoch: int = 100, load_pth: str | None = None) -> None:
-    ddpm = DDPM(eps_model=NaiveUnet(3, 3, n_feat=128), betas=(1e-4, 0.02), n_T=1000)
+    ddpm = DDPM(eps_model=NaiveUnet(3, 3, n_feat=128), beta1=1e-4, beta2=0.02, n_T=1000)
 
     if load_pth is not None:
         ddpm.load_state_dict(torch.load("ddpm_cifar.pth"))
@@ -31,27 +31,23 @@ def train_cifar10(n_epoch: int = 100, load_pth: str | None = None) -> None:
     optim = torch.optim.Adam(ddpm.parameters(), lr=1e-5)
 
     class SaveSample(Callback):
+        @torch.no_grad()
         def on_epoch_end(self, epoch: int, log: dict[str, float | list[float]]) -> None:
             ddpm: DDPM = self.model
             ddpm.eval()
-            with torch.no_grad():
-                xh = ddpm.sample(8, (3, 32, 32))
+            xh = ddpm.sample(8, (3, 32, 32))
             xset = xh
             # xset = torch.cat([xh, x[:8]], dim=0)
             grid = make_grid(xset, normalize=True, value_range=(-1, 1), nrow=4)
             save_image(grid, f"./contents/ddpm_sample_cifar{epoch}.png")
 
-            # save model
-            torch.save(ddpm.state_dict(), "./ddpm_cifar.pth")
-
     engine.train(
         ddpm,
         train_data=dataset,
-        criterion=torch.nn.MSELoss(),
+        criterion=loss,
         batch_size=512,
         optimizer=optim,
         epochs=n_epoch,
-        preprocess_data=preprocess_data,
         callbacks=[SaveSample(), ModelCheckpoint("./ddpm_cifar.pth")],
     )
 
